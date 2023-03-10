c10c9e235007d500802cb2914e23b9a3352a1895
sarahecongu <sarahatimecongu@gmail.com>
2023-03-10 16:06:45 +0300

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,38 @@
+import React ,{useState}from 'react';
+import { CreateTask } from './CreateTask';
+
+const TodoList = () => {
+
+    const [modal,setModal] = useState(false);
+    const[taskList,setTaskList] = useState([])
+    const toggle = ()=>{
+        setModal(!modal);
+    }
+    
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList",tempList)
+        setTaskList(tempList)
+        setModal(false)
+    }
+
+
+    
+    return (
+        <>
+        <div className='header text-center'>
+            <h3 >Todo List</h3>
+            <button className='btn btn-primary mt-2'onClick={()=>setModal
+            (true)}>Create Task</button>
+        </div>
+
+        <div className='task-container'>
+            {taskList.map((obj) =><li>{obj.Name}</li>)}
+
+        </div>
+        <CreateTask toggle ={toggle} modal ={modal} save = { saveTask }/>
+        </>
+    )
+};
+export {TodoList }
